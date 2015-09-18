b5fb2cd85d69ff60201d8d048351310863df7e91
Brendan Molloy <brendan@bbqsrc.net>
2015-09-18 21:34:48 +1000

    let's do this

---
+++ spec/routerSpec.js
@@ -0,0 +1,111 @@
+'use strict';
+
+var Router = require('../lib/router');
+var Route = require('../lib/route');
+
+var koa = require('koa');
+var supertest = require('supertest');
+
+supertest.Test.prototype.exec = function(done) {
+  this.end(function(err) {
+    if (err) {
+      done.fail(err);
+    } else {
+      done();
+    }
+  });
+};
+
+describe('Router', function() {
+  it('can be created', function() {
+    let router = new Router;
+    expect(router).not.toBe(null);
+  });
+
+  it('supports #route', function() {
+    let router = new Router;
+
+    let route = router.route('/any/route');
+
+    expect(route instanceof Route).toBe(true);
+  });
+
+  it('allows #pre setting for all methods', function() {
+    let router = new Router;
+
+    expect(router.preMiddleware['all']).not.toBeDefined();
+    router.pre(function*() {});
+    expect(router.preMiddleware['all']).toBeDefined();
+  });
+
+  it('allows #pre setting for each HTTP method', function() {
+    let router = new Router;
+
+    for (let method of ['get', 'post', 'delete', 'put']) {
+      expect(router.preMiddleware[method]).not.toBeDefined();
+      router.pre(method, function*() {});
+      expect(router.preMiddleware[method]).toBeDefined();
+    }
+  });
+});
+
+describe('Router in an app', function() {
+  var app, request, router;
+
+  beforeAll(function() {
+    app = koa();
+
+    router = new Router;
+
+    router.route('/test', {
+      get: function*() {
+        this.body = 'get!';
+      },
+      post: function*() {
+        this.body = 'post!';
+      },
+      put: function*() {
+        this.body = 'put!';
+      },
+      delete: function*() {
+        this.body = 'delete!';
+      }
+    });
+
+    app.use(router.middleware());
+
+    request = supertest.agent(app.listen());
+  });
+
+  it('returns a body from GET', function(done) {
+    request
+    .get('/test')
+    .expect(200)
+    .expect('get!')
+    .exec(done);
+  });
+
+  it('returns a body from POST', function(done) {
+    request
+    .post('/test')
+    .expect(200)
+    .expect('post!')
+    .exec(done);
+  });
+
+  it('returns a body from PUT', function(done) {
+    request
+    .put('/test')
+    .expect(200)
+    .expect('put!')
+    .exec(done);
+  });
+
+  it('returns a body from DELETE', function(done) {
+    request
+    .delete('/test')
+    .expect(200)
+    .expect('delete!')
+    .exec(done);
+  });
+});
